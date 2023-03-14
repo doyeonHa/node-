225c82953d0bcaa78b54d3f40a09bfe6a919bf2a
doyeonHa <w2113@e-mirim.hs.kr>
2023-03-14 20:06:05 +0900

    2023.03.14 node.js 첫 수업

---
+++ node1/letEx1.js
@@ -0,0 +1,14 @@
+//let과 var의 차이점 : let은 똑같은 변수 선언 불가능, var은 똑같은 변수 선언 가능
+
+let fruit = 'apple';
+console.log('fruit', fruit);
+
+fruit = 'grape';
+console.log('fruit', fruit);
+
+//let fruit = 'hello';
+
+var variable1 = 'hello';
+var variable1 = 'bye';
+
+console.log(variable1);
